d33341bde1d16fae9b851ba957d0b3cf22446257
grantcjones <gjonesyoda@byui.edu>
2023-01-07 22:20:48 -0700

    Submitting Prep1 and Prep2 for W01 Prove

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is your first name?: ");
+        string first_n = Console.ReadLine();
+
+        Console.WriteLine("What is your last name?: ");
+        string last_n = Console.ReadLine();
+
+        Console.WriteLine($"Your name is {last_n}, {first_n} {last_n}.");
     }
 }
+
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade percentage: ");
+        string userInput = Console.ReadLine();
+        int gradePercentage = int.Parse(userInput);
+
+        string letter = "";
+
+        if (gradePercentage >= 90)
+        {
+            letter = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letter = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letter = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letter = "D";
+        }
+        else if (gradePercentage < 60)
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your letter grade is: {letter}");
+
+        if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations! You have satisfactorily passed this class.");
+        }
+        else if (gradePercentage < 70)
+        {
+            Console.WriteLine("Unfortunately, you have not passed this class, and will need to re-enroll to try again.");
+        }
     }
 }
