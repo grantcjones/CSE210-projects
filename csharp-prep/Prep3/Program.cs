54f6f6fa5578de69368c08df15454afd6d7fe397
grantcjones <gjonesyoda@byui.edu>
2023-01-09 11:39:58 -0700

    Completing Prep 3 Assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Console.WriteLine("What is the magic number?: ");
+        string userInputNumber = Console.ReadLine();
+        int magicNumber = int.Parse(userInputNumber);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, magicNumber);
+
+        Console.WriteLine("What is your guess?: ");
+        string userInputGuess = Console.ReadLine();
+        int magicGuess = int.Parse(userInputGuess);
+
+        int tryCounter = 0;
+
+        while (magicGuess != number)
+        {
+            if (magicGuess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicGuess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            tryCounter = (tryCounter + 1);
+
+            Console.WriteLine("What is your guess?: ");
+            magicGuess = int.Parse(Console.ReadLine());
+        }
+
+        if (magicGuess == number)
+        {
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Number of attemps: {tryCounter}.");
+        }
+
     }
 }
