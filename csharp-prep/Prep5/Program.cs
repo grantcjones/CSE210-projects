21e146b50ed0df988900a6cfe0d98cf0a7216617
grantcjones <gjonesyoda@byui.edu>
2023-01-10 10:31:43 -0700

    Prep 5 assignment

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -2,8 +2,46 @@ using System;
 
 class Program
 {
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+        
+        return favoriteNumber;
+    }
+
+    static int SquareNumber(int number)
+    {
+        int numberSquared = (number ^ 2);
+        return numberSquared;
+    }
+
+    static void DisplayResult(string name, int number)
+    {
+        Console.WriteLine($"{name}, the square of your number is {number}");
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string name = PromptUserName();
+        int factor = PromptUserNumber();
+        int finalNumber = SquareNumber(factor);
+        DisplayResult(name, finalNumber);
+
     }
 }
