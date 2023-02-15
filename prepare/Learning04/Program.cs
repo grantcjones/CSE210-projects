bf2babbf14dfbbe9d6d87f4dc8993bec45afd3c0
grantcjones <gjonesyoda@byui.edu>
2023-02-15 14:39:30 -0700

    Learning 4 Assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("John", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+        Console.WriteLine("");
+
+        MathAssignment m1 = new MathAssignment("John", "Arithmetic", "9.8", "1-4");
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeWorkList());
+        
+        Console.WriteLine("");
+
+        WritingAssignment w1 = new WritingAssignment("John", "Literature", "Herman Melville", "Jeff Stamos");
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+        
     }
+
 }
